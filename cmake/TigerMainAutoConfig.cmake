208b7705e802f8c40c7322e47d89ee222f0f659e
Xiling Huang <wisherhxl@gmail.com>
2025-03-22 18:29:06 +0800

    fix: empty base when no proto file.

--- cmake/TigerMainAutoConfig.cmake
+++ cmake/TigerMainAutoConfig.cmake
@@ -2,6 +2,8 @@ set(TI_BASE_MODULE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/modules/base)
 set(TI_BASE_INCLUDE_DIR ${TI_BASE_MODULE_DIR}/include/${TI_INTERNAL_NAME})
 set(TI_BASE_HEADER_DIR ${TI_BASE_INCLUDE_DIR}/base)
 ti_create_directory(${TI_BASE_HEADER_DIR})
+set(TI_BASE_SOURCE_DIR ${TI_BASE_MODULE_DIR}/src)
+ti_create_directory(${TI_BASE_SOURCE_DIR})
 
 configure_file(
   ${CMAKE_CURRENT_SOURCE_DIR}/cmake/templates/ti_base_cmake_lists.txt.in
@@ -15,6 +17,9 @@ configure_file(${CMAKE_CURRENT_SOURCE_DIR}/cmake/templates/ti_def.h.in
 
 configure_file(${CMAKE_CURRENT_SOURCE_DIR}/cmake/templates/types_c.h.in
                ${TI_BASE_HEADER_DIR}/types_c.h @ONLY)
+
+configure_file(${CMAKE_CURRENT_SOURCE_DIR}/cmake/templates/types_c.cpp.in
+               ${TI_BASE_SOURCE_DIR}/types_c.cpp @ONLY)
 
 configure_file(${CMAKE_CURRENT_SOURCE_DIR}/cmake/templates/ti_version.h.in
                ${TI_BASE_HEADER_DIR}/ti_version.h @ONLY)
