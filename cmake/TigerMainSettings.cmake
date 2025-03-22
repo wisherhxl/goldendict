289c94343b2fe37f150a590e23a06c9b65bf7a97
Xiling Huang <wisherhxl@gmail.com>
2025-03-22 12:05:25 +0800

    opt: remove CMakeLists.txt in modules and apps.

--- cmake/TigerMainSettings.cmake
+++ cmake/TigerMainSettings.cmake
@@ -3,7 +3,9 @@ string(TOLOWER "${TI_PROJECT_NAME}" TI_INTERNAL_NAME)
 include(cmake/TigerUtilsExtend.cmake)
 include(cmake/TigerMainAutoConfig.cmake)
 
-set(CMAKE_USE_RELATIVE_PATHS ON CACHE INTERNAL "" FORCE)
+set(CMAKE_USE_RELATIVE_PATHS
+    ON
+    CACHE INTERNAL "" FORCE)
 
 ti_cmake_eval(DEBUG_PRE ONCE)
 
@@ -11,15 +13,27 @@ ti_clear_vars(TigerModules_TARGETS)
 
 include(cmake/TigerDownload.cmake)
 
-set(BUILD_LIST "" CACHE STRING "Build only listed modules (comma-separated, e.g. 'base,sim,opt,...')")
-set(BUILD_APP_LIST "" CACHE STRING "Build only listed applications (comma-separated, e.g. 'example,tester,...')")
+set(BUILD_LIST
+    ""
+    CACHE STRING
+          "Build only listed modules (comma-separated, e.g. 'base,sim,opt,...')"
+)
+set(BUILD_APP_LIST
+    ""
+    CACHE
+      STRING
+      "Build only listed applications (comma-separated, e.g. 'example,tester,...')"
+)
 
 # ----------------------------------------------------------------------------
 # Break in case of popular CMake configuration mistakes
 # ----------------------------------------------------------------------------
 if(NOT CMAKE_SIZEOF_VOID_P GREATER 0)
-  message(FATAL_ERROR "CMake fails to determine the bitness of the target platform.
-  Please check your CMake and compiler installation. If you are cross-compiling then ensure that your CMake toolchain file correctly sets the compiler details.")
+  message(
+    FATAL_ERROR
+      "CMake fails to determine the bitness of the target platform.
+  Please check your CMake and compiler installation. If you are cross-compiling then ensure that your CMake toolchain file correctly sets the compiler details."
+  )
 endif()
 
 # ----------------------------------------------------------------------------
@@ -34,42 +48,49 @@ ti_cmake_hook(POST_DETECT_COMPILER)
 
 # 3rd party libs
 
-
 # Optional 3rd party components
 # ===================================================
 
-
 # Tiger build components
 # ===================================================
-TI_OPTION(BUILD_SHARED_LIBS        "Build shared libraries (.dll/.so) instead of static ones (.lib/.a)" ON )
-TI_OPTION(BUILD_TIGER_APPS        "Build utility applications (used for example or test)" ON )
-TI_OPTION(BUILD_TESTS           "Build all tests"                          OFF )
-set(ENABLE_PRECOMPILED_HEADERS 1) #Use precompiled headers 
+ti_option(BUILD_SHARED_LIBS
+          "Build shared libraries (.dll/.so) instead of static ones (.lib/.a)"
+          ON)
+ti_option(BUILD_TIGER_APPS
+          "Build utility applications (used for example or test)" ON)
+ti_option(BUILD_TESTS "Build all tests" OFF)
+set(ENABLE_PRECOMPILED_HEADERS 1) # Use precompiled headers
 
 # Tiger installation options
 # ===================================================
-TI_OPTION(INSTALL_CREATE_DISTRIB   "Change install rules to build the distribution package" OFF )
-TI_OPTION(INSTALL_TESTS            "Install accuracy and performance test binaries and test data" OFF )
+ti_option(INSTALL_CREATE_DISTRIB
+          "Change install rules to build the distribution package" OFF)
+ti_option(INSTALL_TESTS
+          "Install accuracy and performance test binaries and test data" OFF)
 
 # Tiger build options
 # ===================================================
-TI_OPTION(TIGER_ENABLE_MEMORY_SANITIZER "Better support for memory/address sanitizers"         OFF)
-TI_OPTION(TIGER_WARNINGS_ARE_ERRORS "Treat warnings as errors"                                 OFF )
-TI_OPTION(TIGER_ENABLE_MEMALIGN     "Enable posix_memalign or memalign usage" ON )
-TI_OPTION(ENABLE_CONFIG_VERIFICATION "Fail build if actual configuration doesn't match requested (WITH_XXX != HAVE_XXX)" OFF )
-TI_OPTION(ENABLE_SOLUTION_FOLDERS    "Solution folder in Visual Studio"        ON )
-TI_OPTION(TIGER_CMAKE_DEBUG_MESSAGES "Enable debug messages for Tiger CMake scripts" OFF)
-
+ti_option(TIGER_ENABLE_MEMORY_SANITIZER
+          "Better support for memory/address sanitizers" OFF)
+ti_option(TIGER_WARNINGS_ARE_ERRORS "Treat warnings as errors" OFF)
+ti_option(TIGER_ENABLE_MEMALIGN "Enable posix_memalign or memalign usage" ON)
+ti_option(
+  ENABLE_CONFIG_VERIFICATION
+  "Fail build if actual configuration doesn't match requested (WITH_XXX != HAVE_XXX)"
+  OFF)
+ti_option(ENABLE_SOLUTION_FOLDERS "Solution folder in Visual Studio" ON)
+ti_option(TIGER_CMAKE_DEBUG_MESSAGES
+          "Enable debug messages for Tiger CMake scripts" OFF)
 
 # ----------------------------------------------------------------------------
-#  Get actual Tiger version number from sources
+# Get actual Tiger version number from sources
 # ----------------------------------------------------------------------------
 include(cmake/TigerVersion.cmake)
 
 ti_cmake_hook(POST_OPTIONS)
 
 # ----------------------------------------------------------------------------
-#  Build & install layouts
+# Build & install layouts
 # ----------------------------------------------------------------------------
 
 if(TIGER_TEST_DATA_PATH)
@@ -77,13 +98,16 @@ if(TIGER_TEST_DATA_PATH)
 endif()
 
 # Save libs and executables in the same place
-set(EXECUTABLE_OUTPUT_PATH "${CMAKE_BINARY_DIR}/bin" CACHE PATH "Output directory for applications")
+set(EXECUTABLE_OUTPUT_PATH
+    "${CMAKE_BINARY_DIR}/bin"
+    CACHE PATH "Output directory for applications")
 
-set(LIBRARY_OUTPUT_PATH                "${${PROJECT_NAME}_BINARY_DIR}/lib")
-ti_update(3P_LIBRARY_OUTPUT_PATH      "${${PROJECT_NAME}_BINARY_DIR}/3rdparty/lib")
+set(LIBRARY_OUTPUT_PATH "${${PROJECT_NAME}_BINARY_DIR}/lib")
+ti_update(3P_LIBRARY_OUTPUT_PATH "${${PROJECT_NAME}_BINARY_DIR}/3rdparty/lib")
 
 # Postfix of DLLs:
-ti_update(TIGER_DLLVERSION "${TIGER_VERSION_MAJOR}${TIGER_VERSION_MINOR}${TIGER_VERSION_PATCH}")
+ti_update(TIGER_DLLVERSION
+          "${TIGER_VERSION_MAJOR}${TIGER_VERSION_MINOR}${TIGER_VERSION_PATCH}")
 ti_update(TIGER_DEBUG_POSTFIX d)
 
 if(DEFINED CMAKE_DEBUG_POSTFIX)
@@ -93,18 +117,24 @@ endif()
 include(cmake/TigerInstallLayout.cmake)
 
 # ----------------------------------------------------------------------------
-#  Path for build/platform -specific headers
+# Path for build/platform -specific headers
 # ----------------------------------------------------------------------------
-ti_update(TIGER_CONFIG_FILE_INCLUDE_DIR "${CMAKE_BINARY_DIR}/" CACHE PATH "Where to create the platform-dependant ti_config.h")
+ti_update(TIGER_CONFIG_FILE_INCLUDE_DIR "${CMAKE_BINARY_DIR}/" CACHE PATH
+          "Where to create the platform-dependant ti_config.h")
 ti_include_directories(${TIGER_CONFIG_FILE_INCLUDE_DIR})
 
 # ----------------------------------------------------------------------------
-#  Path for additional modules
+# Path for additional modules
 # ----------------------------------------------------------------------------
-set(TIGER_EXTRA_MODULES_PATH "" CACHE PATH "Where to look for additional Tiger modules (can be ;-separated list of paths)")
+set(TIGER_EXTRA_MODULES_PATH
+    ""
+    CACHE
+      PATH
+      "Where to look for additional Tiger modules (can be ;-separated list of paths)"
+)
 
 # ----------------------------------------------------------------------------
-#  Autodetect if we are in a GIT repository
+# Autodetect if we are in a GIT repository
 # ----------------------------------------------------------------------------
 find_host_package(Git QUIET)
 
@@ -115,12 +145,13 @@ elseif(NOT DEFINED TIGER_VCSVERSION)
   set(TIGER_VCSVERSION "unknown")
 endif()
 
-
 # ----------------------------------------------------------------------------
 # Tiger compiler and linker options
 # ----------------------------------------------------------------------------
-# In case of Makefiles if the user does not setup CMAKE_BUILD_TYPE, assume it's Release:
-if(CMAKE_GENERATOR MATCHES "Makefiles|Ninja" AND "${CMAKE_BUILD_TYPE}" STREQUAL "")
+# In case of Makefiles if the user does not setup CMAKE_BUILD_TYPE, assume it's
+# Release:
+if(CMAKE_GENERATOR MATCHES "Makefiles|Ninja" AND "${CMAKE_BUILD_TYPE}" STREQUAL
+                                                 "")
   set(CMAKE_BUILD_TYPE Release)
 endif()
 
@@ -132,16 +163,16 @@ include(cmake/TigerCompilerOptions.cmake)
 ti_cmake_hook(POST_COMPILER_OPTIONS)
 
 # ----------------------------------------------------------------------------
-#       CHECK FOR SYSTEM LIBRARIES, OPTIONS, ETC..
+# CHECK FOR SYSTEM LIBRARIES, OPTIONS, ETC..
 # ----------------------------------------------------------------------------
 include(CheckIncludeFile)
 include(CheckSymbolExists)
 
 if(TIGER_ENABLE_MEMALIGN)
-    CHECK_INCLUDE_FILE(malloc.h HAVE_MALLOC_H)
-    if(HAVE_MALLOC_H)
-        CHECK_SYMBOL_EXISTS(_aligned_malloc malloc.h HAVE_WIN32_ALIGNED_MALLOC)
-    endif()
+  check_include_file(malloc.h HAVE_MALLOC_H)
+  if(HAVE_MALLOC_H)
+    check_symbol_exists(_aligned_malloc malloc.h HAVE_WIN32_ALIGNED_MALLOC)
+  endif()
 endif()
 
 include(cmake/TigerPCHSupport.cmake)
@@ -149,32 +180,26 @@ include(cmake/TigerModule.cmake)
 include(cmake/TigerApp.cmake)
 
 # ----------------------------------------------------------------------------
-#  Detect endianness of build platform
+# Detect endianness of build platform
 # ----------------------------------------------------------------------------
 include(TestBigEndian)
 test_big_endian(WORDS_BIGENDIAN)
 
 # ----------------------------------------------------------------------------
-#  Detect 3rd-party libraries
+# Detect 3rd-party libraries
 # ----------------------------------------------------------------------------
-find_package(OpenGL REQUIRED)
+# find_package(OpenGL REQUIRED)
 
-# include(cmake/TigerFindDevTools.cmake)
-# include(cmake/TigerFindCameraDrivers.cmake)
-# include(cmake/TigerFindAxisDrivers.cmake)
 include(cmake/TigerFindProtobuf.cmake)
-include(cmake/TigerFindQt.cmake)
-# include(cmake/TigerFindOpenCV.cmake)
-# include(cmake/TigerFindBoost.cmake)
-# include(cmake/TigerFindVTK.cmake)
-include(cmake/TigerFindlog4cplus.cmake)
-
+# include(cmake/TigerFindQt.cmake) include(cmake/TigerFindOpenCV.cmake)
+# include(cmake/TigerFindBoost.cmake) include(cmake/TigerFindVTK.cmake)
+# include(cmake/TigerFindlog4cplus.cmake)
 
 # ----------------------------------------------------------------------------
-#  Detect other 3rd-party libraries/tools
+# Detect other 3rd-party libraries/tools
 # ----------------------------------------------------------------------------
 
-ti_cmake_hook(POST_DETECT_DEPENDECIES)  # typo, deprecated (2019-06)
+ti_cmake_hook(POST_DETECT_DEPENDECIES) # typo, deprecated (2019-06)
 ti_cmake_hook(POST_DETECT_DEPENDENCIES)
 
 # ----------------------------------------------------------------------------
@@ -199,9 +224,9 @@ ti_add_modules_compiler_options()
 add_subdirectory(proto)
 
 # Tiger modules
-add_subdirectory(modules)
+ti_assemble_modules()
 
-# extra applications
+# Tiger applications
 if(BUILD_TIGER_APPS)
-  add_subdirectory(apps)
+  ti_assemble_applications()
 endif()
--- cmake/TigerUtilsExtra.cmake
+++ cmake/TigerUtilsExtra.cmake
@@ -24,3 +24,49 @@ function(ti_get_date_components year month)
       ${DATE_MONTH}
       PARENT_SCOPE)
 endfunction()
+
+macro(ti_assemble_modules)
+  add_definitions(-D__TIGER_BUILD=1)
+
+  if(NOT TIGER_MODULES_PATH)
+    set(TIGER_MODULES_PATH "${${PROJECT_NAME}_SOURCE_DIR}/modules")
+  endif()
+
+  # build lists of modules to be documented
+  set(TIGER_MODULES_MAIN "")
+  set(TIGER_MODULES_EXTRA "")
+
+  foreach(mod ${TIGER_MODULES_BUILD} ${TIGER_MODULES_DISABLED_USER}
+              ${TIGER_MODULES_DISABLED_AUTO} ${TIGER_MODULES_DISABLED_FORCE})
+    string(REGEX REPLACE "^${TI_INTERNAL_NAME}_" "" mod "${mod}")
+    if("${TIGER_MODULE_${TI_INTERNAL_NAME}_${mod}_LOCATION}" STREQUAL
+       "${${PROJECT_NAME}_SOURCE_DIR}/modules/${mod}")
+      list(APPEND TIGER_MODULES_MAIN ${mod})
+    else()
+      list(APPEND TIGER_MODULES_EXTRA ${mod})
+    endif()
+  endforeach()
+  ti_list_sort(TIGER_MODULES_MAIN)
+  ti_list_sort(TIGER_MODULES_EXTRA)
+
+  if(NOT TI_FIXED_ORDER_MODULES)
+    set(TI_FIXED_ORDER_MODULES base)
+  endif()
+  set(FIXED_ORDER_MODULES ${TI_FIXED_ORDER_MODULES})
+  list(REMOVE_ITEM TIGER_MODULES_MAIN ${FIXED_ORDER_MODULES})
+  set(TIGER_MODULES_MAIN ${FIXED_ORDER_MODULES} ${TIGER_MODULES_MAIN})
+
+  set(TIGER_MODULES_MAIN
+      ${TIGER_MODULES_MAIN}
+      CACHE INTERNAL "List of main modules" FORCE)
+  set(TIGER_MODULES_EXTRA
+      ${TIGER_MODULES_EXTRA}
+      CACHE INTERNAL "List of extra modules" FORCE)
+endmacro()
+
+macro(ti_assemble_applications)
+  if(NOT TIGER_APPLICATIONS_PATH)
+    set(TIGER_APPLICATIONS_PATH "${${PROJECT_NAME}_SOURCE_DIR}/apps")
+  endif()
+  ti_glob_applications(${TIGER_APPLICATIONS_PATH})
+endmacro()
