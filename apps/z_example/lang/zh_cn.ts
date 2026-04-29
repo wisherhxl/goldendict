9b1c12834c2d4e2f4eadc41eac9d8e666d8873d3
Xiling Huang <wisherhxl@gmail.com>
2026-04-29 21:32:22 +0800

    feat: update project configuration and add Chinese translation support

---
+++ apps/z_example/lang/zh_cn.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>z_example</name>
+    <message>
+        <source>Rago Qt Example</source>
+        <translation>Rago Qt 案例</translation>
+    </message>
+    <message>
+        <source>Qt example is running with %1 and proto message %2.</source>
+        <translation>Qt 案例 %1 %2。</translation>
+    </message>
+    <message>
+        <source>Close (%1)</source>
+        <translation>关闭 %1</translation>
+    </message>
+</context>
+</TS>
--- apps/z_example/src/main.cpp
+++ apps/z_example/src/main.cpp
@@ -1,20 +1,48 @@
 #include <QApplication>
+#include <QCoreApplication>
+#include <QDir>
 #include <QLabel>
 #include <QPushButton>
+#include <QTranslator>
 #include <QVBoxLayout>
 #include <QWidget>
+
+#include "tiger/test.pb.h"
+#include "tiger/ti_base.tp.h"
+
+#include <QString>
 
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
 
+    QTranslator translator;
+    const QString translationDir = QCoreApplication::applicationDirPath() + QDir::separator() + "lang";
+    if (translator.load("zh_cn", translationDir)) {
+        app.installTranslator(&translator);
+    }
+
+    rago::TestRequest request;
+    request.set_id("z_example");
+    request.set_name("Tiger Qt Example");
+    request.add_tags("module");
+    request.add_tags("proto");
+
+    const int wrappedTagIndex = tiWrap(request.tags_size(), 0, 9);
+
     QWidget window;
-    window.setWindowTitle("Rago Qt Example");
+    window.setWindowTitle(QApplication::translate("z_example", "Rago Qt Example"));
     window.resize(360, 180);
 
     auto* layout = new QVBoxLayout(&window);
-    auto* label = new QLabel("Qt example is running.", &window);
-    auto* button = new QPushButton("Close", &window);
+    auto* label = new QLabel(
+        QApplication::translate("z_example", "Qt example is running with %1 and proto message %2.")
+            .arg(TI_PROJECT_NAME)
+            .arg(QString::fromStdString(request.ShortDebugString())),
+        &window);
+    auto* button = new QPushButton(&window);
+
+    button->setText(QApplication::translate("z_example", "Close (%1)").arg(wrappedTagIndex));
 
     label->setAlignment(Qt::AlignCenter);
     layout->addWidget(label);
